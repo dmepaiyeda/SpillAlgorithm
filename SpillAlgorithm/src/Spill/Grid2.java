d9e3a8a623ef3e40e78deb9426d2ca3dccd4a3f6
Deborah Mepaiyeda <dmepaiyeda@gmail.com>
2018-03-17 11:12:50 -0400

    Update Grid2.java

--- SpillAlgorithm/src/Spill/Grid2.java
+++ SpillAlgorithm/src/Spill/Grid2.java
@@ -221,14 +221,15 @@ public class Grid {
     			if (!isCell(row, col)){ //if cell is out of bounds
     				return; 
     			}
-    			if (grid[row][col].isObstacle()){ //if cell is an obstacle
+    			Cell acell = grid[row][col];
+    			if (acell.isObstacle()){ //if cell is an obstacle
     				return;
     			}
     			
-    			if (grid[row][col].getValue() >= strength){
+    			if (acell.getValue() >= strength){
     				return;
     			}
-    			grid[row][col].setValue(strength);
+    			acell.setValue(strength);
     			
     			
     			Spill(row-1,col-1,strength-1);
